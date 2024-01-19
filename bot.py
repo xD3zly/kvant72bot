36f0df52148d7c6cdd2844159c5fefbedbbeb131
ripshit <ddnissenbaum@gmail.com>
2024-01-19 16:08:06 +0500

    add button and send alarm

--- bot.py
+++ bot.py
@@ -7,18 +7,35 @@ bot = telebot.TeleBot('6415511856:AAFDpvJ-2YsryCM_fv09CnYPBqOMiMlqpbg')
 @bot.message_handler(commands=['start'])
 def start(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, 'здравсствуйте! какое направление вы хотите выбрать?')
+    bot.send_message(chat_id, 'Здравствуйте! Какое направление вы хотите выбрать?', reply_markup=get_markup())
+
+
+def get_markup():
     markup = types.InlineKeyboardMarkup()
-    btnIt = types.InlineKeyboardButton('айти')
-    btnrobo = types.InlineKeyboardButton('робо')
-    btnavto = types.InlineKeyboardButton('авто')
-    btnaer = types.InlineKeyboardButton('аэро')
-    btnIdk = types.InlineKeyboardButton('я не знаю')
-    markup.row(btnIt, btnrobo, btnavto, btnaer, btnIdk)
+    btnIt = types.InlineKeyboardButton('Айти', callback_data='it')
+    btnrobo = types.InlineKeyboardButton('Робо', callback_data='robo')
+    btnavto = types.InlineKeyboardButton('Авто', callback_data='auto')
+    btnaer = types.InlineKeyboardButton('Аэро', callback_data='aero')
+    btnIdk = types.InlineKeyboardButton('Я не знаю', callback_data='idk')
+    markup.add(btnIt, btnrobo, btnavto, btnaer, btnIdk)
+    return markup
 
 
-def direction(message):
-    None
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'it':
+        bot.answer_callback_query(call.id, 'Вы выбрали Айти' )
 
+    elif call.data == 'robo':
+        bot.answer_callback_query(call.id, 'Вы выбрали Робо')
+
+    elif call.data == 'auto':
+        bot.answer_callback_query(call.id, 'Вы выбрали Авто')
+
+    elif call.data == 'aero':
+        bot.answer_callback_query(call.id, 'Вы выбрали Аэро')
+
+    elif call.data == 'idk':
+        bot.answer_callback_query(call.id, 'Если вы не знаете какое напрвление вам выбрать предлогаем пройти тест!')
 
 bot.polling(none_stop=True)
